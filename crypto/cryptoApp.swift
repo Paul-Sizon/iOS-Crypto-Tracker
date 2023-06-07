4ab7c62f55acca6c70259ef62fc5fcff552a02b6
psizonenko <psizonenko@griddynamics.com>
2023-06-07 17:00:58 +0300

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by Pavel Sizonenko on 07.06.23.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
